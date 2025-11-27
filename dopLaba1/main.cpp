998dfb37ddb3060baf596c6fd369fd8c16a8c423
Maxoniii <maxkirilenkobest@gmail.com>
2025-11-27 22:42:27 +0200

    Update main.cpp

--- dopLaba1/main.cpp
+++ dopLaba1/main.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
-
-
+#include <climits>
 int main() {
-	unsigned long long h = 257;
-	std::cout << "Number: " << h << std::endl;
-	std::cout << "Size: " << sizeof(h) << " bytes" << std::endl;
-
-
-	unsigned char* byte = (unsigned char*)&h;
-
-
-	std::cout << "Bytes: ";
-	for (int i = 0; i < sizeof(h); i++) {
-		std::cout << static_cast<int>(byte[i]) << " ";
+	long long q1, p1, q2, p2, A;
+	std::cout << "Input values of tracks and their prices: " << std::endl;
+	std::cin >> q1 >> p1 >> q2 >> p2 >> A;
+	if ((q1 <= 0) || (p1 <= 0) || (q2 <= 0) || (p2 <= 0)) {
+		std::cout << "Values must be more than 0" << std::endl;
+		return 1;
 	}
-	std::cout << std::endl;
+	if ((q1 > 1000) || (p1 > 1000) || (q2 > 1000) || (p2 > 1000) || (A > 1000)) {
+		std::cout << "Values must be less than 1000" << std::endl;
+		return 1;
+	}
 
+	long long min_p = LLONG_MAX;
+	long long j = (A + q1 - 1) / q1 + 10;
+	long long max_tr1 = j;
+	if (j > 1000) {
+		max_tr1 = 1000;
+	}
+	for (long long tr1 = 0; tr1 <= max_tr1; tr1++) {
+		long long ton1 = tr1 * q1;
+		long long ost_ton = A - ton1;
+
+		if (ost_ton <= 0) {
+			long long p = tr1 * p1;
+			if (p < min_p) {
+				min_p = p;
+			}
+		}
+		else {
+			long long tr2 = (ost_ton + q2 - 1) / q2;
+			long long final_p = tr1 * p1 + tr2 * p2;
+			if (final_p < min_p) {
+				min_p = final_p;
+			}
+		}
+	}
+	std::cout << min_p << std::endl;
 	return 0;
 }
