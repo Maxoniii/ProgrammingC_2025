5580c4d325ade6728c427e89eccdcc00bc13e323
Maxoniii <maxkirilenkobest@gmail.com>
2026-05-23 12:08:20 +0200

    Update main.cpp

--- lab11.3/main.cpp
+++ lab11.3/main.cpp
@@ -1101,3 +1101,230 @@ int main() {
     return 0;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "matrix.hpp"
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+
+namespace mt {
+
+void matrix::_swap(matrix& other) {
+    std::swap(str_, other.str_);
+    std::swap(slb_, other.slb_);
+    std::swap(data_, other.data_);
+}
+
+matrix::matrix() : str_(0), slb_(0), data_(nullptr) {}
+
+matrix::matrix(size_t str, size_t slb) : str_(str), slb_(slb), data_(new double[str * slb]) {
+    std::fill(data_, data_ + str * slb, 0.0);
+}
+
+matrix::~matrix() {
+    if (data_ != nullptr) {
+        delete[] data_;
+        data_ = nullptr;
+    }
+}
+
+matrix::matrix(const matrix& other)
+    : str_(other.str_), slb_(other.slb_), data_(new double[str_ * slb_]) {
+    std::copy(other.data_, other.data_ + str_ * slb_, data_);
+}
+
+matrix& matrix::operator=(matrix other) {
+    _swap(other);
+    return *this;
+}
+
+double& matrix::sh(size_t str, size_t slb) {
+    if (str >= str_ || slb >= slb_) {
+        std::cout << "Error: Escape from matrix" << std::endl;
+        static double zagl = 0.0;
+        return zagl;
+    }
+    return data_[str * slb_ + slb];
+}
+
+const double& matrix::sh(size_t str, size_t slb) const {
+    if (str >= str_ || slb >= slb_) {
+        std::cout << "Error: Escape from matrix" << std::endl;
+        static const double zagl = 0.0;
+        return zagl;
+    }
+    return data_[str * slb_ + slb];
+}
+
+void matrix::input() {
+    if (is_empty()) {
+        std::cout << "Matrix is empty, nothing to input" << std::endl;
+        return;
+    }
+    for (size_t i = 0; i < str_; ++i) {
+        for (size_t j = 0; j < slb_; ++j) {
+            std::cin >> sh(i, j);
+        }
+    }
+}
+
+void matrix::print() const {
+    if (is_empty()) {
+        std::cout << "Matrix is empty" << std::endl;
+        return;
+    }
+    for (size_t i = 0; i < str_; i++) {
+        for (size_t j = 0; j < slb_; j++) {
+            std::cout << sh(i, j) << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+void matrix::swap_with(matrix& other) {
+    _swap(other);
+}
+
+void swap(matrix& a, matrix& b) {
+    a.swap_with(b);
+}
+
+matrix& matrix::operator++() {
+    for (size_t i = 0; i < str_ * slb_; ++i) {
+        data_[i] += 1.0;
+    }
+    return *this;
+}
+
+matrix matrix::operator++(int) {
+    matrix temp(*this);
+    ++(*this);
+    return temp;
+}
+
+matrix operator+(const matrix& lhs, const matrix& rhs) {
+    if (lhs.get_str() != rhs.get_str() || lhs.get_slb() != rhs.get_slb()) {
+        throw std::invalid_argument("Размеры матрицы должны совпадать для сложения");
+    }
+    matrix result(lhs.get_str(), lhs.get_slb());
+    for (size_t i = 0; i < lhs.get_str(); ++i) {
+        for (size_t j = 0; j < lhs.get_slb(); ++j) {
+            result.sh(i, j) = lhs.sh(i, j) + rhs.sh(i, j);
+        }
+    }
+    return result;
+}
+
+matrix operator*(const matrix& lhs, const matrix& rhs) {
+    matrix result(lhs);
+    result *= rhs;
+    return result;
+}
+
+matrix& matrix::operator*=(const matrix& rhs) {
+    if (this->get_slb() != rhs.get_str()) {
+        throw std::invalid_argument("Количество столбцов левой матрицы должно совпадать с количеством строк правой");
+    }
+    matrix result(this->get_str(), rhs.get_slb());
+    for (size_t i = 0; i < this->get_str(); ++i) {
+        for (size_t j = 0; j < rhs.get_slb(); ++j) {
+            double sum = 0.0;
+            for (size_t k = 0; k < this->get_slb(); ++k) {
+                sum += this->sh(i, k) * rhs.sh(k, j);
+            }
+            result.sh(i, j) = sum;
+        }
+    }
+    *this = result;
+    return *this;
+}
+
+matrix matrix::get_minor_matrix(size_t str, size_t slb) const {
+    matrix result(this->get_str() - 1, this->get_slb() - 1);
+    size_t t_i = 0;
+    for (size_t i = 0; i < this->get_str(); ++i) {
+        if (i == str) continue;
+
+        size_t t_j = 0;
+        for (size_t j = 0; j < this->get_slb(); ++j) {
+            if (j == slb) continue;
+            result.sh(t_i, t_j) = this->sh(i, j);
+            t_j++;
+        }
+        t_i++;
+    }
+    return result;
+}
+
+double matrix::determinate() const {
+    if (this->get_str() != this->get_slb()) {
+        throw std::logic_error("Определитель считается только для квадратных матриц");
+    }
+    
+    size_t n = this->get_str();
+
+    if (n == 0) return 0.0;
+    if (n == 1) return this->sh(0, 0);
+
+    if (n == 2) {
+        return (this->sh(0, 0) * this->sh(1, 1)) - (this->sh(0, 1) * this->sh(1, 0));
+    }
+
+    if (n == 3) {
+        double glavn_elem = (this->sh(0, 0) * this->sh(1, 1) * this->sh(2, 2)) + 
+                            (this->sh(1, 0) * this->sh(2, 1) * this->sh(0, 2)) + 
+                            (this->sh(0, 1) * this->sh(1, 2) * this->sh(2, 0));
+        
+        double poboch_elem = (this->sh(0, 2) * this->sh(1, 1) * this->sh(2, 0)) + 
+                             (this->sh(0, 1) * this->sh(1, 0) * this->sh(2, 2)) + 
+                             (this->sh(1, 2) * this->sh(2, 1) * this->sh(0, 0));
+        return glavn_elem - poboch_elem;
+    }
+
+    double deter = 0.0;
+    for (size_t j = 0; j < n; ++j) {
+        deter += this->sh(0, j) * this->algebr_dop(0, j);
+    }
+
+    return deter;
+}
+
+double matrix::algebr_dop(size_t i, size_t j) const {
+    if (this->get_str() != this->get_slb()) {
+        throw std::logic_error("Алгебраическое дополнение считается только для квадратных матриц");
+    }
+    
+    double sign = ((i + j) % 2 == 0) ? 1.0 : -1.0;
+    return sign * this->get_minor_matrix(i, j).determinate();
+}
+
+} // namespace mt
+
